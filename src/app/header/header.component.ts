ec917a3ab0d153262adc345fd803343971d05c27
Lexsak <bartoszpiprek123@gmail.com>
2023-11-17 17:58:11 +0100

    header section

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,12 @@ import { Subscription } from "rxjs";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
   private userSub: Subscription;
+  isSaveDataDisabled: boolean = true;
 
   constructor(
     private dataStoreService: DataStorageService,
